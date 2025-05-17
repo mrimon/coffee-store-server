6816130a145d034d363cddca41747b5bc3d8583f
mrimon <rmamun1585@gmail.com>
2025-05-17 22:54:56 +0600

    modified some code

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
 require('dotenv').config()
 const app = express();
@@ -28,12 +28,48 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const coffeeCollection = client.db('coffeeDb').collection('coffees')
+
+    app.get('/coffees', async(req, res) => {
+        const result = await coffeeCollection.find().toArray();
+        res.send(result)
+    });
+    app.get('/coffees/:id', async(req, res) => {
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)};
+        const result = await coffeeCollection.findOne(query);
+        res.send(result)
+    })
+    app.post('/coffees', async(req, res) => {
+        const newCoffee = req.body;
+        const result = await coffeeCollection.insertOne(newCoffee);
+        res.send(result)
+        console.log(newCoffee);
+    });
+    app.put('/coffees/:id', async(req, res) => {
+        const id = req.params.id;
+        const filter = { _id: new ObjectId(id)};
+        const updatedCoffee = req.body;
+        const updatedDoc = {
+            $set : updatedCoffee
+        };
+        const result = await coffeeCollection.updateOne(filter, updatedDoc);
+        res.send(result);
+    })
+    app.delete('/coffees/:id', async(req, res) => {
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)};
+        const result = await coffeeCollection.deleteOne(query);
+        res.send(result)
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
